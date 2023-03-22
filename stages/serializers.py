a4ac2678d1847d8053672ab0f815060dc66e4b16
Moshood <moshoodakeemolayinka@gmail.com>
2023-03-22 09:00:04 +0100

    modify serialiser reverse function

--- stages/serializers.py
+++ stages/serializers.py
@@ -6,38 +6,45 @@ from steps.serializers import StepSerializer
 
 class StageSerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField(read_only=True)
-    initial_stage = serializers.SerializerMethodField(read_only=True)
-    final_stage = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Stage
-        fields = ['url', 'name', 'initial_stage', 'final_stage']
+        fields = ['url', 'name', 'is_initial_stage', 'is_final_stage']
 
     def get_url(self, obj):
         request = self.context.get('request')
-        url = reverse('stage-detail', args=[obj.process.id, obj.id], request=request)
+        print(obj.name)
+        url = reverse('stage-detail', args=[obj.process.name, obj.name], request=request)
         return url
-    
-    def get_initial_stage(self, obj):
-        return obj.is_initial_stage
 
-    def get_final_stage(self, obj):
-        return obj.is_final_stage
 
 
     
 class StageDetailSerializer(serializers.ModelSerializer):
-    actions = serializers.SerializerMethodField(read_only=True)
+    steps = serializers.SerializerMethodField(read_only=True)
+    permissions = serializers.SerializerMethodField(read_only=True)
+    groups = serializers.SerializerMethodField(read_only=True)
 
 
     class Meta:
         model = Stage
-        fields = ['actions']
+        fields = ['steps', 'permissions', 'groups']
 
 
-    def get_actions(self, obj):
+    def get_steps(self, obj):
         steps = obj.step_set.all()
         request = self.context.get('request')
         steps_data = StepSerializer(steps, many=True, context={'request': request}).data
         return steps_data
+    
+    def get_permissions(self, obj):
+        perms = obj.permissions.all()
+        permsissions = [{permission.id : permission.name} for permission in perms]
+        return permsissions
+    
+
+    def get_groups(self, obj):
+        groups = obj.groups.all()
+        groups = [{group.id : group.name} for group in groups]
+        return groups
     
