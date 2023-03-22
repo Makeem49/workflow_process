c02652417b88bc9f5544ab9eb7aecea302a2c235
Moshood <moshoodakeemolayinka@gmail.com>
2023-03-22 08:56:57 +0100

    restructure url

--- app/urls.py
+++ app/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('process/', include('process.urls')),
-    path('process/<str:process_id>/stages/', include('stages.urls')),
-    path('process/<str:process_id>/stage/<str:stage_id>/steps/', include('steps.urls')),
+    path('process/<str:process_name>/stages/', include('stages.urls')),
+    path('process/<str:process_name>/stage/<str:stage_name>/steps/', include('steps.urls')),
     path('grant/', include('grant_access.urls'))
 ]
