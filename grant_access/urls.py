e5cfe5fd4d56fbb2a07a99c185fe0a3ca1713ec2
Moshood <moshoodakeemolayinka@gmail.com>
2023-03-22 08:58:07 +0100

    add add url

--- grant_access/urls.py
+++ grant_access/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('permission/<str:process_name>/<str:stage_name>/<int:pk>/', \
             views.AssignStagePermissionView.as_view(), name='assign-permission-to-stage'),
     path('permission/<str:process_name>/<str:stage_name>/<str:step_name>/<int:pk>/', \
-            views.AssignActionPermissionView.as_view(), name='assign-permission-to-steps'),
+            views.AssignStepPermissionView.as_view(), name='assign-permission-to-steps'),
     path('permission/<str:process_name>/<int:pk>/', \
             views.AssignProcessPermissionView.as_view(), name='assign-permission-to-process'),
 
@@ -22,5 +22,11 @@ urlpatterns = [
     path('group/update/<int:pk>/', views.GrantGroupUpdateView.as_view(), name='group-update'),
     path('group/delete/<int:pk>/', views.GrantGroupDeleteView.as_view(), name='group-delete'),
 
-    path('group/<str:group_name>/<int:pk>/', views.AssignGroupPermissionView.as_view(), name='assign-permission-to-group')
+    path('group/<str:group_name>/<int:pk>/', views.AssignGroupPermissionView.as_view(), name='assign-permission-to-group'),
+
+    path('group/<str:process_name>/add/<int:pk>/', views.AssignGroupProcessView.as_view(), name='assign-group-to-process'),
+
+    path('group/<str:process_name>/<str:stage_name>/add/<int:pk>/', views.AssignGroupStageView.as_view(), name='assign-group-to-stage'),
+
+    path('group/<str:process_name>/<str:stage_name>/<str:step_name>/add/<int:pk>/', views.AssignGroupStepView.as_view(), name='assign-group-to-step'),
 ]
