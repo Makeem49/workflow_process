37fdf40f7e113bee978c51eee48dd91fc7d04f06
Moshood <moshoodakeemolayinka@gmail.com>
2023-03-22 09:01:13 +0100

    update endpoint to perform search operation

--- stages/urls.py
+++ stages/urls.py
@@ -4,7 +4,7 @@ from stages import views
 urlpatterns = [
     path('create/', views.StageCreateView.as_view(), name='stage-create'),
     path('', views.StageListView.as_view(), name='stage-list'),
-    path('<int:pk>/', views.StageDetailView.as_view(), name='stage-detail'),
+    path('<str:stage_name>/', views.StageDetailView.as_view(), name='stage-detail'),
     path('<int:pk>/update', views.StageUpdateView.as_view(), name='stage-update'),
     path('<int:pk>/deactivate/', views.StageDeactivateView.as_view(), name='stage-deactivate'),
 ]
