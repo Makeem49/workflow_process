861507a596c4624f5f3f62857202dc1d865f0ec1
Moshood <moshoodakeemolayinka@gmail.com>
2023-03-22 09:01:49 +0100

    update views

--- stages/views.py
+++ stages/views.py
@@ -16,8 +16,12 @@ class StageCreateView(generics.CreateAPIView):
 
 
     def create(self, request, *args, **kwargs):
-        process_id = self.kwargs['process_id']
-        process = get_object_or_404(Process, pk=process_id)
+        process_name = self.kwargs['process_name']
+        process = Process.objects.filter(name__iexact=process_name).first()
+        
+        if not process:
+            raise NotFound(detail='Process not found', code=status.HTTP_404_NOT_FOUND)
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(process=process)
@@ -29,18 +33,20 @@ class StageListView(generics.ListAPIView):
     serializer_class = StageSerializer
 
     def get_queryset(self):
-        process_id = self.kwargs['process_id']
-        process = get_object_or_404(Process, pk=process_id)
+        process_name = self.kwargs['process_name']
+        process = Process.objects.filter(name__iexact=process_name).first()
+        if not process:
+            raise NotFound(detail='Process not found', code=status.HTTP_404_NOT_FOUND)
         queryset = Stage.objects.filter(process=process)
         return queryset
     
     def get_serializer_context(self):
-        process_id = self.kwargs['process_id']
+        process_name = self.kwargs['process_name']
         return {
             'request': self.request,
             'process_name': self.format_kwarg,
             'view': self,
-            'process_name' : process_id
+            'process_name' : process_name
         }
 
 
@@ -50,24 +56,33 @@ class StageDetailView(generics.RetrieveAPIView):
     lookup_url_kwarg = ['process_id']
 
     def get_object(self):
-        process_id = self.kwargs['process_id']
-        stage_id = self.kwargs['pk']
-        process = get_object_or_404(Process, pk=process_id)
-        stage = get_object_or_404(Stage, pk=stage_id)
-        print(stage)
+        stage_name = self.kwargs['stage_name']
+        process_name = self.kwargs['process_name']
+        process = Process.objects.filter(name__iexact=process_name).first()
+
+        if not process:
+            raise NotFound(detail='Process not found', code=status.HTTP_404_NOT_FOUND)
+        
+        stage = Stage.objects.filter(name=stage_name, process=process).first()
+
         return stage
 
 
-class StageUpdateView(generics.UpdateAPIView):
+class StageUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Stage.objects.all()
     serializer_class = StageSerializer
     lookup_field='pk'
 
     def get_object(self):
-        process_id = self.kwargs['process_id']
         stage_id = self.kwargs['pk']
-        process = get_object_or_404(Process, pk=process_id)
-        stage = get_object_or_404(Stage, pk=stage_id)
+        process_name = self.kwargs['process_name']
+        process = Process.objects.filter(name__iexact=process_name).first()
+        
+        if not process:
+            raise NotFound(detail='Process not found', code=status.HTTP_404_NOT_FOUND)
+        
+        stage = Stage.objects.filter(id=stage_id, process=process).first()
+
         return stage
 
 
@@ -77,8 +92,13 @@ class StageDeactivateView(generics.DestroyAPIView):
     lookup_field='pk'
 
     def get_object(self):
-        process_id = self.kwargs['process_id']
         stage_id = self.kwargs['pk']
-        process = get_object_or_404(Process, pk=process_id)
-        stage = get_object_or_404(Stage, pk=stage_id)
+        process_name = self.kwargs['process_name']
+        process = Process.objects.filter(name__iexact=process_name).first()
+        
+        if not process:
+            raise NotFound(detail='Process not found', code=status.HTTP_404_NOT_FOUND)
+        
+        stage = Stage.objects.filter(id=stage_id, process=process).first()
+
         return stage
